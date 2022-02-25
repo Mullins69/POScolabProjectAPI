948c37cb5e50ef628247889ff401b631a97e38b6
GitRoss2021 <lilithamantini@gmail.com>
2022-02-25 11:12:34 +0200

    testing

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const mongoose = require("mongoose");
-const contactRoute = require('./routes/contactAPI')
+const contactRoute = require("./routes/contactAPI");
 const userRouter = require("./routes/userRouter");
 const productRouter = require("./routes/productRouter");
 // Setting up MongoDB connection
@@ -17,136 +17,138 @@ app.use(express.json());
 
 // API routes
 app.get("/", (req, res, next) => {
-    res.send({
-      message: "Welcome to Abduls and Lilitas API",
-      user_routes: {
-        user_register: {
-          method: "POST",
-          route: "/users",
-          request_body: {
-            fullname: "String",
-            email: "String",
-            phone_number: "String",
-            password: "String",
-            cart: "object"
-          },
-          result: {
-            jwt: "String token",
-          },
+  res.send({
+    message: "Welcome to Abduls and Lilitas API",
+    user_routes: {
+      user_register: {
+        method: "POST",
+        route: "/users",
+        request_body: {
+          fullname: "String",
+          email: "String",
+          phone_number: "String",
+          password: "String",
+          cart: "object",
         },
-        user_login: {
-          method: "PATCH",
-          route: "/users",
-          request_body: {
-            email: "String",
-            password: "String",
-          },
-          result: {
-            jwt: "String token",
-          },
-        },
-        all_users: {
-          method: "GET",
-          route: "/users",
-          result: {
-            users: "Array",
-          },
-        },
-        single_user: {
-          method: "GET",
-          route: "/users/:id",
-          result: {
-            user: "Object",
-          },
-        },
-        update_user: {
-          method: "PUT",
-          request_body: {
-            fullname: "String",
-            email: "String",
-            phone_number: "String",
-            password: "String",
-            img: "String *optional* (Must be hosted image. I can suggest to host on Post Image)",
-            cart: "object"
-          },
-          route: "/users/:id",
-          result: {
-            user: "Object",
-          },
-        },
-        delete_user: {
-          method: "DELETE",
-          route: "/users/:id",
-          result: {
-            message: "Object",
-          },
+        result: {
+          jwt: "String token",
         },
       },
-      product_routes: {
-        all_products: {
-          method: "GET",
-          route: "/products",
-          headers: {
-            authorization: "Bearer (JWT token)",
-          },
-          result: {
-            product: "Array",
-          },
+      user_login: {
+        method: "PATCH",
+        route: "/users",
+        request_body: {
+          email: "String",
+          password: "String",
         },
-        single_product: {
-          method: "GET",
-          route: "/postproducts/:id",
-          headers: {
-            authorization: "Bearer (JWT token)",
-          },
-          result: {
-            product: "Object",
-          },
-        },
-        create_product: {
-          method: "POST",
-          route: "/posts/",
-          headers: {
-            authorization: "Bearer (JWT token)",
-          },
-          request_body: {
-            title: "String",
-            body: "String",
-            img: "String *optional* (Must be hosted image. I can suggest to host on Post Image)",
-          },
-          result: {
-            post: "Object",
-          },
-        },
-        update_product: {
-          method: "PUT",
-          route: "/products/:id",
-          headers: {
-            authorization: "Bearer (JWT token)",
-          },
-          request_body: {
-            title: "String *optional*",
-            body: "String *optional*",
-            img: "String *optional* (Must be hosted image. I can suggest to host on Post Image)",
-          },
-          result: {
-            post: "Object",
-          },
-        },
-        delete_post: {
-          method: "DELETE",
-          route: "/posts/:id",
-          result: {
-            message: "Object",
-          },
+        result: {
+          jwt: "String token",
         },
       },
-    });
+      all_users: {
+        method: "GET",
+        route: "/users",
+        result: {
+          users: "Array",
+        },
+      },
+      single_user: {
+        method: "GET",
+        route: "/users/:id",
+        result: {
+          user: "Object",
+        },
+      },
+      update_user: {
+        method: "PUT",
+        request_body: {
+          fullname: "String",
+          email: "String",
+          phone_number: "String",
+          password: "String",
+          img: "String *optional* (Must be hosted image. I can suggest to host on Post Image)",
+          cart: "object",
+        },
+        route: "/users/:id",
+        result: {
+          user: "Object",
+        },
+      },
+      delete_user: {
+        method: "DELETE",
+        route: "/users/:id",
+        result: {
+          message: "Object",
+        },
+      },
+    },
+    product_routes: {
+      all_products: {
+        method: "GET",
+        route: "/products",
+        headers: {
+          authorization: "Bearer (JWT token)",
+        },
+        result: {
+          product: "Array",
+        },
+      },
+      single_product: {
+        method: "GET",
+        route: "/postproducts/:id",
+        headers: {
+          authorization: "Bearer (JWT token)",
+        },
+        result: {
+          product: "Object",
+        },
+      },
+      create_product: {
+        method: "POST",
+        route: "/posts/",
+        headers: {
+          authorization: "Bearer (JWT token)",
+        },
+        request_body: {
+          title: "String",
+          body: "String",
+          img: "String *optional* (Must be hosted image. I can suggest to host on Post Image)",
+        },
+        result: {
+          post: "Object",
+        },
+      },
+      update_product: {
+        method: "PUT",
+        route: "/products/:id",
+        headers: {
+          authorization: "Bearer (JWT token)",
+        },
+        request_body: {
+          title: "String *optional*",
+          body: "String *optional*",
+          img: "String *optional* (Must be hosted image. I can suggest to host on Post Image)",
+        },
+        result: {
+          post: "Object",
+        },
+      },
+      delete_post: {
+        method: "DELETE",
+        route: "/posts/:id",
+        result: {
+          message: "Object",
+        },
+      },
+    },
   });
+});
 app.use("/users", userRouter);
-app.use('/contact', contactRoute);
-app.use('/products', productRouter)
-app.set('port', process.env.PORT || 6969);
-app.listen(app.get('port'), server =>{
-    console.info(`Server listen on port ${app.get('port')}`);
-})
+app.use("/contact", contactRoute);
+app.use("/products", productRouter);
+app.set("port", process.env.PORT || 6969);
+app.listen(app.get("port"), (server) => {
+  console.info(`Server listen on port ${app.get("port")}`);
+});
+
+console.log("hello Alex");
